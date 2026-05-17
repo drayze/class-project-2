ef1da256953bccbc346410e9152bf0b4544ef838
drayze <slinkardpaul50@gmail.com>
2026-05-16 22:37:56 -0500

    feat: Add callback handler to AnswerButton
    This commit updates the `AnswerButton` widget to support interaction by adding a required callback function.
    
    Key changes include:
    - **`lib/answer_button.dart`**:
        - Updated the constructor to require an `onTap` callback function and changed `answerText` to a named parameter.
        - Added a `void Function()` property named `onTap`.
        - Updated the `ElevatedButton`'s `onPressed` handler to execute the provided `onTap` callback.

--- lib/answer_button.dart
+++ lib/answer_button.dart
@@ -1,13 +1,15 @@
 import 'package:flutter/material.dart';
 
 class AnswerButton extends StatelessWidget{
-  const AnswerButton(this.answerText, {super.key});
+  const AnswerButton({super.key, required this.answerText,
+    required this.onTap});
 
   final String answerText;
+  final void Function() onTap;
 
   @override
   Widget build(BuildContext context){
-    return ElevatedButton(onPressed: (){},
+    return ElevatedButton(onPressed: onTap,
         style: ElevatedButton.styleFrom(
           backgroundColor: Colors.purpleAccent,),
         child: Text(answerText));
