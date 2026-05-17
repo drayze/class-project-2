11399de6171ca88e8957632e70307a39e24ab228
drayze <slinkardpaul50@gmail.com>
2026-05-16 22:14:11 -0500

    feat: Define initial quiz questions data
    This commit introduces the initial set of quiz questions by creating a data file and populating it with a sample question.
    
    Key changes include:
    - **`lib/data/questions.dart`**:
        - Added an import for the `QuizQuestion` model.
        - Defined a `questions` constant containing a list of `QuizQuestion` objects, starting with a question about Flutter building blocks.

---
+++ lib/data/questions.dart
@@ -0,0 +1,6 @@
+import 'package:class_project_2/Models/quiz_question.dart';
+
+const questions = [
+  QuizQuestion('What are the main building blocks of Flutter apps?',
+    ['Widgets', 'Containers', 'Views', 'Fragments'],)
+];
